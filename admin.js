703400540ff770fe3ba5aac90d26f211422a6f88
Arafat Bin Ayub <69151073+arafatbinayub@users.noreply.github.com>
2026-07-09 23:34:24 +0600

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,25 @@
+function login(){
+
+const username=document.getElementById("username").value.trim();
+const password=document.getElementById("password").value.trim();
+const msg=document.getElementById("msg");
+
+if(username==="admin" && password==="12345"){
+
+msg.style.color="green";
+msg.innerHTML="Login Successful...";
+
+setTimeout(function(){
+
+window.location.href="dashboard.html";
+
+},1000);
+
+}else{
+
+msg.style.color="red";
+msg.innerHTML="Wrong Username or Password";
+
+}
+
+}
