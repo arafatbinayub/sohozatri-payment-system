fcab77d7868c6a9642052b22a038c405dbe92aaf
Arafat Bin Ayub <69151073+arafatbinayub@users.noreply.github.com>
2026-07-10 12:31:50 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,25 +1,30 @@
-function login(){
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwsehHUQEVbLUtRdw3dGkP_2Pt5u_Al5UdWnEr4GrSEnlQ36QtCSaxJ-NF23fSVFjP_Qw/exec";
 
-const username=document.getElementById("username").value.trim();
-const password=document.getElementById("password").value.trim();
-const msg=document.getElementById("msg");
+function login() {
 
-if(username==="sohozatri" && password==="sohozatri26"){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-msg.style.color="green";
-msg.innerHTML="Login Successful...";
-
-setTimeout(function(){
-
-window.location.href="dashboard.html";
-
-},1000);
-
-}else{
-
-msg.style.color="red";
-msg.innerHTML="Wrong Username or Password";
-
-}
+    fetch(WEB_APP_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            action: "login",
+            username: username,
+            password: password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.success) {
+            localStorage.setItem("admin","true");
+            location.href="dashboard.html";
+        } else {
+            document.getElementById("msg").innerHTML="❌ Invalid Username or Password";
+        }
+    })
+    .catch(err=>{
+        alert(err);
+        console.log(err);
+    });
 
 }
